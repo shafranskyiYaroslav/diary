b930d3d688263bdd0dab8744dd0873919cdf4b75
ShafranskyiYaroslav <yshafranskyi@intellias.com>
2020-01-09 11:53:05 +0200

    Updates

---
+++ precache-manifest.baa28830ad4fa27e919517f3661cdf9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a40317b3400c1a29df336b7cf3399b",
+    "url": "/diary/index.html"
+  },
+  {
+    "revision": "f8ee43567aad579d7632",
+    "url": "/diary/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "1e185ff36a3341ff2488",
+    "url": "/diary/static/css/main.f672e67f.chunk.css"
+  },
+  {
+    "revision": "f8ee43567aad579d7632",
+    "url": "/diary/static/js/2.a077c61d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/diary/static/js/2.a077c61d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e185ff36a3341ff2488",
+    "url": "/diary/static/js/main.5f626ca0.chunk.js"
+  },
+  {
+    "revision": "fb31b017ab16fcd517a6",
+    "url": "/diary/static/js/runtime-main.2ee46f3a.js"
+  }
+]);
